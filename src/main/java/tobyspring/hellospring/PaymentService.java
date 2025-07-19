aa4e3cf331e2f322cfafc025c2388b7117abd484
godsun7892 <godsun7892@naver.com>
2025-07-19 12:20:40 +0900

    상속을 통한 확장

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -11,29 +11,10 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
-public class PaymentService {
+public abstract class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-
-        // 환율 가져오기
-        // https://open.er-api.com/v6/latest/
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        // InputStream() -> 가져온 데이터를 바이트형식으로 보내줌
-        // InputStreamReader() -> 바이트 형식을 케릭터로 바꿔줌
-        // BufferedReader() -> 한 줄씩 가져옴
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = data.rates().get("KRW");
-
-        // 금액 계산
+        BigDecimal exRate = getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효 시간 계산
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return Payment.builder()
@@ -46,10 +27,6 @@ public class PaymentService {
                 .build();
     }
 
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
-    }
+    abstract BigDecimal getExRate(String currency) throws IOException;
 
 }
