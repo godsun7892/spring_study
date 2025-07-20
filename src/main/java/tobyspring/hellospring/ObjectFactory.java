6ea06df55397f15d893893b7444e9ca592f30b49
godsun7892 <godsun7892@naver.com>
2025-07-20 10:27:30 +0900

    어노테이션을 이용하여 객체들 가져오기

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,10 +1,14 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
+
+    /*
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
@@ -13,5 +17,6 @@ public class ObjectFactory {
     public ExRateProvider exRateProvider(){
         return new WebApiExRateProvider();
     }
+    */
 
 }
